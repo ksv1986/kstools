53cbeaa228f6ea0547caefa889fb20c7f4ebb249
ksv1986 <ksv0x07c2@gmail.com>
2022-12-26 23:10:36 +0100

    Add jpeg

---
+++ jpeg.py
@@ -0,0 +1,53 @@
+from .types import ImageSize, ImageSizeResult, PreadStream, b2x
+from typing import IO
+
+
+def be16(data: bytes):
+    return int.from_bytes(data[:2], "big")
+
+
+class JpegParser:
+    def __init__(self, stream: IO[bytes]):
+        self.stream = PreadStream(stream)
+
+    def image_size(self) -> ImageSizeResult:
+        soi = self.stream.read(2)
+        if soi[0] != 0xff or soi[1] != 0xd8:
+            return (None, f"Wrong SOI {b2x(soi)}")
+
+        while True:
+            offs = self.stream.offs
+
+            seg = self.stream.read(2)
+            if len(seg) < 2:
+                return (None, f"EOF")
+
+            if seg[0] != 0xff:
+                return (None, f"Wrong segment header {b2x(seg)} at {offs}")
+
+            if seg[1] >= 0xd0 and seg[1] < 0xd8: # RSTn; skip
+                continue
+
+            if seg[1] == 0xd9:
+                return ((None, f"EOI"), None, None)
+
+            seg_len = self.stream.read(2)
+            if len(seg_len) < 2:
+                return (None, f"EOF")
+
+            skip = be16(seg_len) - 2
+            # print(f"seg: offs={offs:x}/{offs} {b2x(seg)}{b2x(seg_len)} len={skip}")
+            if seg[1] == 0xc0 or seg[1] == 0xc2: # SOFn
+                data = self.stream.read(5)
+                if len(data) < 5:
+                    return (None, f"EOF")
+
+                h = be16(data[1:3])
+                w = be16(data[3:5])
+                return (ImageSize(w, h), None)
+
+            self.stream.skip(skip)
+
+
+def jpeg_image_size(stream: IO[bytes]) -> ImageSizeResult:
+    return JpegParser(stream).image_size()
